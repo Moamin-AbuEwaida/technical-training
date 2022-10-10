d53c1e40d2e176c69c3442d125d0abb29e2b8a4a
Moamin <moamin.salamah@gmail.com>
2022-10-10 15:13:10 +0200

    adding attributes to the fields

--- estate/models.py
+++ estate/models.py
@@ -1,13 +1,16 @@
-from zoneinfo import available_timezones
 from odoo import fields, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class TestModel(models.Model):
     _name = "test.model"
     _description = "Test Model"
 
+   
+
     name = fields.Char()
-    selling_price = fields.Integer()
-    available_dates = fields.Date()
-    number_of_rooms = fields.Integer()
+    selling_price = fields.Integer(readonly=True, copy=False)
+    available_dates = fields.Date(copy=False, default=datetime.today() - relativedelta(months=3) )
+    number_of_rooms = fields.Integer(default=2)
 
     
