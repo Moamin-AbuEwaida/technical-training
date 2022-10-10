7757c4c5c54cf42343ec0bea634bc32bd83b9e05
Moamin <moamin.salamah@gmail.com>
2022-10-10 14:02:37 +0200

    refining the menu part and adding new fields fof the app

--- estate/models.py
+++ estate/models.py
@@ -1,3 +1,4 @@
+from zoneinfo import available_timezones
 from odoo import fields, models
 
 class TestModel(models.Model):
@@ -5,3 +6,8 @@ class TestModel(models.Model):
     _description = "Test Model"
 
     name = fields.Char()
+    selling_price = fields.Integer()
+    available_dates = fields.DateTimeField()
+    number_of_rooms = fields.Integer()
+    
+    
