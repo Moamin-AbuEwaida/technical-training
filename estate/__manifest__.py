bb0248dbbd6d728df054d1780a5bbe107bc30c59
Moamin <moamin.salamah@gmail.com>
2022-10-10 12:05:33 +0200

    adding estate-property-views file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,9 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        'estate_property_views.xml',
     ],
+   
     "installable": True,
     'license': 'LGPL-3',
 }
--- estate/models.py
+++ estate/models.py
@@ -2,3 +2,6 @@ from odoo import models
 
 class TestModel(models.Model):
     _name = "test.model"
+    _description = "Test Model"
+
+    name = fields.Char()
