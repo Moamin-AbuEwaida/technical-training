939b410d1bbc4a504604386236d7c673faee5e87
Moamin <moamin.salamah@gmail.com>
2022-10-11 13:20:15 +0200

    adding search model

--- estate/models.py
+++ estate/models.py
@@ -9,9 +9,9 @@ class TestModel(models.Model):
    
 
     name = fields.Char()
-    selling_price = fields.Integer(readonly=True, copy=False)
-    available_dates = fields.Date(copy=False, default=datetime.today() - relativedelta(months=3) )
-    number_of_rooms = fields.Integer(default=2)
+    selling_price = fields.Integer(string='Selling price',readonly=True, copy=False)
+    available_dates = fields.Date(string='Available Dates',copy=False, default=datetime.today() - relativedelta(months=3) )
+    number_of_rooms = fields.Integer(string='Number of Rooms',default=2)
     active = fields.Boolean(default=False)
     # state = fields.Many2one('New', 'Offer Received' , 'Offer Accepted', 'Sold' , 'Canceled')
     state = fields.Selection([
